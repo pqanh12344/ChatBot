dc657bc167f6bf58bf957b8931c5d6ba6828817c
Phan Quoc Anh <nguyenanh3tk12344@gmail.com>
2025-05-18 17:16:37 +0700

    edit config

--- config.py
+++ config.py
@@ -11,10 +11,10 @@ ANSWERS_PATH = './data/viquad.answers'
 
 # Cấu hình API
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
-API_KEY = 'fbfbfbdbd'  # Thay bằng API key thực tế hoặc sử dụng Streamlit secrets
+API_KEY = 'HUGGINGFACE_API_KEY'  # Thay bằng API key thực tế hoặc sử dụng Streamlit secrets
 
 # Cấu hình mô hình
-MODEL_PATH = './model/halong_embedding'
+MODEL_PATH = 'halong_embedding'
 CHUNK_SIZE = 256
 CHUNK_OVERLAP = 20
 BATCH_SIZE = 4
