1c4b31818411196ad0c52ebf1d263fc5b37920eb
Phan Quoc Anh <nguyenanh3tk12344@gmail.com>
2025-05-20 20:56:15 +0700

    edit config

--- config.py
+++ config.py
@@ -1,8 +1,15 @@
 import logging
+from dotenv import load_dotenv
+import os
 
 # Thiết lập logging
 logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 logger = logging.getLogger(__name__)
+
+load_dotenv()
+HF_API_KEY = os.getenv("HF_API_KEY")
+if not HF_API_KEY:
+    raise ValueError("HF_API_KEY not found")
 
 # Cấu hình file paths
 CONTEXTS_PATH = './data/viquad.contexts'
@@ -11,7 +18,7 @@ ANSWERS_PATH = './data/viquad.answers'
 
 # Cấu hình API
 API_URL = "https://api-inference.huggingface.co/models/mistralai/Mixtral-8x7B-Instruct-v0.1"
-API_KEY = 'HUGGINGFACE_API_KEY'  # Thay bằng API key thực tế hoặc sử dụng Streamlit secrets
+API_KEY = HF_API_KEY  # Thay bằng API key thực tế hoặc sử dụng Streamlit secrets
 
 # Cấu hình mô hình
 MODEL_PATH = 'halong_embedding'
